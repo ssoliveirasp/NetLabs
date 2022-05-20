7ee23c6b3c98ad5eb69cb0636f9b00047b9293dc
Sandro Oliveira <ssoliveira@gmail.com>
2022-05-20 08:23:55 -0300

    Inclusao Pipeline Pattern

---
+++ 3.1/Multithreading/DesignPatterns/Pipeline/TapPipelineLabs/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TplPipelineLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task<int> t1 = Task.Factory.StartNew(() =>
+            { return CreateUser(); });
+
+            var t2 = t1.ContinueWith((antecedent) =>
+            { return InitiateWorkflow(antecedent.Result); });
+            
+            var t3 = t2.ContinueWith((antecedant) =>
+            { return SendEmail(antecedant.Result); });
+
+            Console.Read();
+        }
+
+        public static int CreateUser()
+        {
+            //Create user, passing hardcoded user ID as 1 
+            Console.WriteLine("Start - Create User");
+            Thread.Sleep(1000);
+            Console.WriteLine("User created \n");
+            return 1;
+        }
+
+        public static int InitiateWorkflow(int userId)
+        {
+            //Initiate Workflow 
+            Console.WriteLine("Start - Initiate Workflow");
+            Thread.Sleep(1000);
+            Console.WriteLine("Workflow initiates \n");
+
+            return userId;
+        }
+
+        public static int SendEmail(int userId)
+        {
+            //Send email 
+            Console.WriteLine("Start - Email Send");
+            Thread.Sleep(1000);
+            Console.WriteLine("Email sent");
+
+            return userId;
+        }
+    }
+}
---
+++ 3.1/Multithreading/DesignPatterns/Pipeline/TapPipelineLabs/Readme.md
@@ -0,0 +1,6 @@
+﻿The pipeline pattern is commonly used in scenarios where we need to execute the asynchronous tasks in sequence:
+
+_____________________________________
+|                                    |
+| ----Tasks1------->Task2----->Task3 |
+|____________________________________|
