0dcb5ec3e7e8ab43ebe15fba416eb2cdd41aac58
Sandro Oliveira <ssoliveira@gmail.com>
2022-06-29 08:34:52 -0300

    AutoResetEvent Project

---
+++ 3.1/Multithreading/Sync/AutoResetEventLabs/AutoResetEventLab.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoResetEventLabs
+{
+    internal class AutoResetEventLab
+    {
+        AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+
+        public void Execute()
+        {
+            Task signallingTask = Task.Factory.StartNew(() => {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine($"[BeforeSignal.{i}]   Task with CurrentId {Task.CurrentId} LoopId: {i} will give signal to enter");
+
+                    if (i != 2)
+                       autoResetEvent.Set();
+                    else
+                    {
+                        Console.WriteLine($"[WithoutSignal.{i}]  Task with id {Task.CurrentId} id: {i} without signal to enter");
+                    }
+                }
+            });
+
+            int sum = 0;
+
+            Parallel.For(1, 5, (i) => {
+                Console.WriteLine($"[WaitSignal.{i}]     Task with CurrentId {Task.CurrentId} LoopId: {i} waiting for signal to enter");
+           
+                autoResetEvent.WaitOne();
+                Console.WriteLine($"[ReceivedSignal.{i}] Task with CurrentId {Task.CurrentId} LoopId: {i} received signal to enter");
+           
+                sum += i;
+            });
+        }
+    }
+}
---
+++ 3.1/Multithreading/Sync/AutoResetEventLabs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace AutoResetEventLabs
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var autoResetLab = new AutoResetEventLab();
+            
+            autoResetLab.Execute();
+        }
+    }
+}
