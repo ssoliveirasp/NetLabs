1add1d0b368a6da19b36559e9244b8690c144ec0
Sandro Oliveira <ssoliveira@gmail.com>
2022-06-29 07:48:26 -0300

    Inclusão SemaphoreWriteFiles

---
+++ 3.1/Multithreading/Sync/SemaphoreWriteFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SemaphoreWriteFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task[] tasksProcess = new Task[20];
+
+            try
+            {
+                foreach (var i in Enumerable.Range(1, tasksProcess.Length))
+                {
+                    tasksProcess[i - 1] = Task.Factory.StartNew(() => new SemaphoreWriteFile($"#{i}").WriteFile());
+                }
+
+                Task.WhenAll(tasksProcess);
+            }
+            catch (AggregateException ex)
+            {
+                foreach (Exception innerException in ex.InnerExceptions)
+                {
+                    Console.WriteLine(innerException.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3.1/Multithreading/Sync/SemaphoreWriteFiles/SemaphoreWriteFile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreWriteFiles
+{
+    internal class SemaphoreWriteFile
+    {
+        private readonly string ProcessName;
+        private readonly Semaphore SemaphoreFile;
+
+        public SemaphoreWriteFile(string processName)
+        {
+            var SemaphoreName = new DirectoryInfo(Directory.GetCurrentDirectory()).Name;
+
+            ProcessName = processName;
+            SemaphoreFile = new Semaphore(initialCount: 1, maximumCount: 5, name: SemaphoreName);
+        }
+        private static object Locker { get; } = new object();
+
+        public void WriteFile()
+        {
+            var range = Enumerable.Range(1, 10);
+
+            Console.WriteLine($"WriteFile started Process: {ProcessName} TaskId: {Task.CurrentId}");
+
+            range.AsParallel().AsOrdered().ForAll(i =>
+            {
+                Thread.Sleep(10);
+
+                SemaphoreFile.WaitOne();
+
+                try
+                {
+                    File.AppendAllLines("testMutex.txt", new[] { $" {DateTime.Now} Process: {ProcessName} Number: {i.ToString()} TaskId: {Task.CurrentId}" });
+                }
+                catch (AggregateException ex)
+                {
+                    Console.WriteLine($"Task has finished with exception {ex.InnerException.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Task has finished with exception {ex.Message}");
+                }
+
+                finally
+                {
+                    SemaphoreFile.Release();
+                }
+            });
+
+            Console.WriteLine($"WriteFile finished process: {ProcessName} TaskId: {Task.CurrentId}");
+
+        }
+    }
+}
