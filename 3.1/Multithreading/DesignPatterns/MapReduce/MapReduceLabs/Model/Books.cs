cd1218906238461987ef6938e80868697d1e8d92
Sandro Oliveira <ssoliveira@gmail.com>
2022-07-12 07:19:06 -0300

    Inclusão MapReduce

---
+++ 3.1/Multithreading/DesignPatterns/MapReduce/MapReduceLabs/Model/Books.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bogus;
+
+namespace MapReduceLabs.Model
+{
+ 
+
+    internal class Books
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public BooksEnum.CategoryEnum Category { get; set; }
+
+
+        public static IEnumerable<Books> CreateListFake()
+        {
+            var category = new[] { BooksEnum.CategoryEnum.Technology, BooksEnum.CategoryEnum.Bussiness, BooksEnum.CategoryEnum.Romance };
+
+            var orderIds = 0;
+            var testOrders = new Faker<Books>()
+                //Ensure all properties have rules. By default, StrictMode is false
+                //Set a global policy by using Faker.DefaultStrictMode
+                .StrictMode(true)
+                //OrderId is deterministic
+                .RuleFor(o => o.Id, f => orderIds++)
+                //Pick some fruit from a basket
+                .RuleFor(o => o.Category, f => f.PickRandom(category))
+                //A random quantity from 1 to 10
+                .RuleFor(o => o.Name, f => f.Lorem.Paragraph());
+
+            return testOrders.Generate(1000).ToList<Books>();
+        }
+    }
+    public class BooksEnum
+    {
+        public enum CategoryEnum
+        {
+            Technology = 1,
+            Bussiness = 2,
+            Romance = 3,
+        }
+    }
+}
